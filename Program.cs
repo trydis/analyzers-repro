3ae423f37b9f1773399cdf62a416176dc26a3db6
Morten Trydal <trydis@hotmail.com>
2019-11-25 09:24:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Analyzers.Repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var stream = new MemoryStream();
+        }
+    }
+}
